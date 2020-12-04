38c751e8382e402c22c157aa4d79d3dce798cce8
Simon Rubuano <sr@mgk.dev>
2020-12-04 19:40:51 +0100

    🔥 Remove test route

--- routes/web.php
+++ routes/web.php
@@ -22,26 +22,3 @@ Route::group(['middleware' => 'auth'], function () {
     Route::post('logout', 'Auth\LoginController@logout')->name('logout');
     Route::get('/', 'HomeController@index')->name('home');
 });
-
-
-Route::get('test', function () {
-    $from = now()->subDays(7)->startOfDay();
-    $to = now();
-    $userId = auth()->user()->id;
-
-    $res = DB::query()
-        ->selectRaw('project, created_at')
-        ->selectRaw('created_at - lag(created_at, 1) over (partition by project, to_char(created_at, \'yyyy-mm-dd\') order by created_at) as diff')
-        ->from('heartbeats')
-        ->whereBetween('created_at', [$from, $to])
-        // ->where('user_id', $userId)
-        ->orderBy('project')
-        ->orderBy('created_at', 'ASC');
-
-    dump(
-        $res
-            ->get()
-            ->where('project', 'actual')
-            ->toArray()
-    );
-});
