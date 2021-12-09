e7261f68129c219d0dfaeb93a75f647dbaad32d6
Simon Rubuano <sr@mgk.dev>
2021-12-09 13:32:43 +0100

    chore: add tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix
-    .js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
-
-    // .postCss('resources/css/app.css', 'public/css', [
-    //     //
-    // ]);
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        require("tailwindcss"),
+    ]);
