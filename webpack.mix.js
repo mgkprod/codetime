5339aa46951e750fa51669dc43ffc6ea4a60d32e
Simon Rubuano <sr@mgk.dev>
2021-12-09 14:02:03 +0100

    chore: move public resources

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ const mix = require("laravel-mix");
 
 mix
     .js("resources/js/app.js", "public/js").vue({ version: 2 })
-    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")]);
+    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
+    .copyDirectory('resources/public/', 'public/');
