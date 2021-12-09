0c89ed4d44c80561454c04e84858210456845739
Simon Rubuano <sr@mgk.dev>
2021-12-09 14:16:44 +0100

    chore: autoload local vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,25 @@ InertiaProgress.init();
 
 Vue.prototype.$route = route;
 
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+const files = require.context('./', true, /\.vue$/i);
+files.keys().map((key) =>
+  Vue.component(
+    key
+      .split('/')
+      .pop()
+      .split('.')[0],
+    files(key).default,
+  ),
+);
+
 createInertiaApp({
     resolve: (name) => require(`./pages/${name}`),
     setup({ el, App, props }) {
