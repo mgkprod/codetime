c259bb7698c06d05ee23619b67dfe9eabc358d3c
Simon Rubuano <sr@mgk.dev>
2021-12-14 23:39:28 +0100

    chore: tailwind 3

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
         <script src="{{ mix('/js/app.js') }}" defer></script>
         @routes
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-    purge: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-    ],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
+  content: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.vue'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  },
+  plugins: [],
 };
